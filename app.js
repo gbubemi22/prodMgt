87272b8ef4005be9ad3f55a163fd302741f6082f
gbubemi22 <slimnfine22@gmail.com>
2023-04-01 12:26:22 +0100

    first push

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+
+const port = process.env.PORT || 4000
+
+
+
+
+app.listen(port, () => {
+     console.log(`listening on port ${port}`)
+})
